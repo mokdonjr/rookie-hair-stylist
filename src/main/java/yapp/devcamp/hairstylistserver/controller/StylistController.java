4a8e0e0ee66dd7bcde57bc50f5d9d35f4f711acd
ekfkfl <shbogu@naver.com>
2017-08-20 06:55:38 +0900

    라우팅 정보

--- src/main/java/yapp/devcamp/hairstylistserver/controller/StylistController.java
+++ src/main/java/yapp/devcamp/hairstylistserver/controller/StylistController.java
@@ -1,17 +1,113 @@
 package yapp.devcamp.hairstylistserver.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import yapp.devcamp.hairstylistserver.model.Shop;
+import yapp.devcamp.hairstylistserver.model.Stylist;
+import yapp.devcamp.hairstylistserver.model.User;
 
 /**
  * Stylist management Controller
  */
 @Controller
+@RequestMapping("/stylist")
 public class StylistController {
-	
+
 	/**
 	 * enroll stylist
+	 * 
+	 * @param :
+	 *            stylist data
+	 * @return : shopList
 	 */
-	public void enroll(){
-		
+	@RequestMapping("/enroll")
+	public ModelAndView enroll(Stylist stylist) {
+		// stylist정보 저장하고
+
+		// shopList를 가져와
+
+		// 메인으로 리턴
+		ModelAndView model = new ModelAndView();
+		model.setViewName("/");
+
+		return model;
 	}
+
+	/**
+	 * redirect apply_form.html
+	 * @param : nickname
+	 * @return : userId
+	 */
+	@RequestMapping("/apply")
+	public ModelAndView apply(User user) {
+		//nickname으로 userId 가져와 apply_form으로 전달
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("apply_form");
+		
+		return model;
+	}
+	
+	/**
+	 * redirect mypage method
+	 * @return : stylist shop, portfolio, postscript data 
+	 */
+	@RequestMapping("/mypage")
+	public ModelAndView mypage(HttpSession session){
+		//session에서 stylistID 가져오기
+		
+		//스타일리스트 샵, 포트폴리오, 후기 정보들을 mypage로 전달
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("mypage");
+		
+		return model;
+	}
+	
+	/**
+	 * redirect mypage
+	 */
+	@RequestMapping("/sellproduct")
+	public String sellproduct(){
+		
+		return "redirect:mypage";
+	}
+	
+	/**
+	 * redirect mypage
+	 */
+	@RequestMapping("/portfolio")
+	public String portfolio(){
+		return "redirect:mypage";
+	}
+	
+	/**
+	 * redirect mypage
+	 */
+	@RequestMapping("/postscript")
+	public String postscript(){
+		return "redirect:mypage";
+	}
+	
+	/**
+	 * redirect to shop.html
+	 * @param : shopCode
+	 * @return : shopData
+	 */
+	@RequestMapping("/shop")
+	public ModelAndView shop(Shop shop){
+		//shopCode로 샵정보 읽어와서
+		
+		//shop.html로 정보 리턴
+		
+		ModelAndView model = new ModelAndView();
+		model.setViewName("shop");
+		
+		return model;
+	}
+
 }
--- src/main/java/yapp/devcamp/hairstylistserver/controller/UserController.java
+++ src/main/java/yapp/devcamp/hairstylistserver/controller/UserController.java
@@ -2,6 +2,9 @@ package yapp.devcamp.hairstylistserver.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import yapp.devcamp.hairstylistserver.model.User;
 
 /**
  * User management Controller
@@ -12,9 +15,57 @@ public class UserController {
 
 	/**
 	 * Login user method
+	 * @param : user login data
 	 */
-	public void login(){
+	@RequestMapping("/login")
+	public String login(User user){
+		//로그인 처리
 		
+		//메인페이지 리턴
+		return "redirect:index";
+	}
+	
+	/**
+	 * Logout user method
+	 */
+	@RequestMapping("/logout")
+	public String logout(){
+		//로그아웃 처리
+		
+		//메인페이지 리턴
+		return "redirect:index";
+	}
+	
+	/**
+	 * return index.html
+	 * @return : shopList
+	 */
+	@RequestMapping("/index")
+	public ModelAndView index(){
+		ModelAndView model = new ModelAndView();
+		
+		//shopList를 가져와서
+		
+		//메인으로 리턴
+		model.setViewName("/");
+		
+		return model;
+	}
+	
+	/**
+	 * Read booking Information method
+	 * @return : booklist
+	 * @param : userId
+	 */
+	@RequestMapping("/orderlist")
+	public ModelAndView bookList(User user){
+		//고객 예약 정보를 가져와
+		
+		//주문조회 페이지로 리턴
+		ModelAndView model = new ModelAndView();
+		model.setViewName("order_list");
+		
+		return model;
 	}
 	
 	/**
